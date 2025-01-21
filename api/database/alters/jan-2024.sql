a9b642b1fe662e5faf9f4fd917ac105a3bf3bd64
Taikhumsf <taikhum.farhullah53@gmail.com>
2025-01-21 14:04:27 +0500

    corrections in querry

--- api/database/alters/jan-2024.sql
+++ api/database/alters/jan-2024.sql
@@ -1,9 +1,9 @@
+
 ALTER TABLE purchase_order
 ADD COLUMN buyer_id CHAR(36) AFTER buyer_name,
-ADD COLUMN ship_to VARCHAR2(255) AFTER buyer_id,
+ADD COLUMN ship_to VARCHAR(255) AFTER buyer_id,
 DROP COLUMN buyer_name,
 DROP COLUMN buyer_email;
 
 ALTER TABLE purchase_order_detail
-ADD COLUMN vpart VARCHAR(255) AFTER description,
-
+ADD COLUMN vpart VARCHAR(255) AFTER description;
