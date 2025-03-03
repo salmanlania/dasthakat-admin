3ba54bded20e07823e60339ca7bab4c109a8be6e
Rayyan Ali <113789630+DevRayyan@users.noreply.github.com>
2025-03-03 16:13:10 +0500

    add column sort order and description

--- api/database/alters/mar-2025.sql
+++ api/database/alters/mar-2025.sql
@@ -1,2 +1,4 @@
-ALTER TABLE job_order_detail_id
-ADD COLUMN sort_order INT(11) AFTER charge_order_detail_id;
+ALTER TABLE job_order_detail
+ADD COLUMN sort_order INT(11) AFTER charge_order_detail_id,
+ADD COLUMN description TEXT ;
+
