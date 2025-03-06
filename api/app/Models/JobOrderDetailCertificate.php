0531624d287ddd7db7ddda55afd67bbc62b9090e
Rayyan Ali <113789630+DevRayyan@users.noreply.github.com>
2025-03-06 13:04:43 +0500

    alter ijo certificate table structure

---
+++ api/app/Models/JobOrderDetailCertificate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JobOrderDetailCertificate extends Model
+{
+    protected $table = 'job_order_detail_certificate';
+    protected $primaryKey = 'certificate_id';
+    public $incrementing = false;
+    protected $fillable = [
+        'certificate_id',
+        'job_order_id',
+        'job_order_detail_id',
+        'certificate_number',
+        'certificate_date',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by'
+    ];
+}
