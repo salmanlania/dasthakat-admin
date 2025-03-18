1bc0948bf4685bba499a0bf584038c3958e7b125
Rayyan Ali <113789630+DevRayyan@users.noreply.github.com>
2025-03-18 11:41:47 +0500

    dispatch columns filter issue

--- api/app/Http/Controllers/EventDispatchController.php
+++ api/app/Http/Controllers/EventDispatchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ChargeOrder;
 use App\Models\EventDispatch;
+use App\Models\Technician;
 
 class EventDispatchController extends Controller
 {
@@ -12,25 +13,33 @@ class EventDispatchController extends Controller
 	{
 		$query = EventDispatch::leftJoin('event as e', 'event_dispatch.event_id', '=', 'e.event_id')
 			->leftJoin('vessel as v', 'e.vessel_id', '=', 'v.vessel_id')
+			->leftJoin('job_order as jo', 'jo.event_id', '=', 'e.event_id')
 			->leftJoin('technician as t', 't.technician_id', '=', 'event_dispatch.technician_id')
 			->leftJoin('agent as a', 'a.agent_id', '=', 'event_dispatch.agent_id')
 			->where('e.company_id', $request->company_id)
 			->where('e.company_branch_id', $request->company_branch_id);
 
 		if ($event_date = $request->input('event_date')) {
-			$query->whereDate('e.created_at', $event_date);
+			$query->whereDate('event_dispatch.event_date', $event_date);
+		}
+		if ($event_time = $request->input('event_time')) {
+			$query->whereTime('event_dispatch.event_time', $event_time);
 		}
 		if ($event_id = $request->input('event_id')) {
-			$query->where('e.event_id', $event_id);
+			$query->where('event_dispatch.event_id', $event_id);
 		}
 		if ($vessel_id = $request->input('vessel_id')) {
 			$query->where('e.vessel_id', $vessel_id);
 		}
-		if ($technician_id = $request->input('technician_id')) {
-			$query->where('event_dispatch.technician_id', $technician_id);
-		}
+
 		if ($agent_id = $request->input('agent_id')) {
 			$query->where('event_dispatch.agent_id', $agent_id);
+		}
+		if ($technician_notes = $request->input('technician_notes')) {
+			$query->where('event_dispatch.technician_notes', 'like', '%' . $technician_notes . '%');
+		}
+		if ($agent_notes = $request->input('agent_notes')) {
+			$query->where('event_dispatch.agent_notes', 'like', '%' . $agent_notes . '%');
 		}
 		if ($search = $request->input('search')) {
 			$search = strtolower($search);
@@ -46,10 +55,20 @@ class EventDispatchController extends Controller
 		$sortColumn = $request->input('sort_column', 'e.created_at');
 		$sortDirection = $request->input('sort_direction') === 'ascend' ? 'asc' : 'desc';
 
+
 		$data = $query
-			->select('event_dispatch.*', 'v.name as vessel_name', 'v.vessel_id', 'e.event_code', 't.technician_id', 't.name as technician_name', 'a.agent_id', 'a.name as agent_name')
+			->select('event_dispatch.*', 'v.name as vessel_name', 'v.vessel_id', 'e.event_code', 'event_dispatch.technician_id', 't.name as technician_name', 'a.agent_id', 'a.name as agent_name','jo.job_order_id')
 			->orderBy($sortColumn, $sortDirection)
 			->paginate($request->input('limit', 10));
+		foreach ($data as $key => $value) {
+
+			$technicianIds = $value->technician_id;
+			if (!is_array($technicianIds) || empty($technicianIds)) {
+				$value->technicians = null;
+			} else {
+				$value->technicians = Technician::whereIn('technician_id', $technicianIds)->get();
+			}
+		}
 
 		return response()->json($data);
 	}
@@ -88,6 +107,9 @@ class EventDispatchController extends Controller
 
 		if ($request->has('event_date')) {
 			$dispatch->event_date = $request->event_date;
+		}
+		if ($request->has('event_time')) {
+			$dispatch->event_time = $request->event_time;
 		}
 		if ($request->has('technician_id')) {
 			$dispatch->technician_id = $request->technician_id;
