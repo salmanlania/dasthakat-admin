f18198a810e498794f4d4692a4af04dcb7464e88
Rayyan Ali <113789630+DevRayyan@users.noreply.github.com>
2025-03-27 14:52:08 +0500

    job order print show DO/SO number

--- api/app/Http/Controllers/JobOrderController.php
+++ api/app/Http/Controllers/JobOrderController.php
@@ -94,6 +94,7 @@ class JobOrderController extends Controller
 		$data = JobOrder::with(
 			"job_order_detail",
 			"job_order_detail.charge_order",
+			"job_order_detail.shipment_detail",
 			"job_order_detail.product",
 			"job_order_detail.product_type",
 			"job_order_detail.unit",
