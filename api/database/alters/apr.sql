69309649898676f8d7edf64099bb1b03af840ce2
Rayyan Ali <113789630+DevRayyan@users.noreply.github.com>
2025-04-08 12:25:22 +0500

    servicelist detail warehouse should be nullable

---
+++ api/database/alters/apr.sql
@@ -0,0 +1,2 @@
+ALTER TABLE servicelist_received_detail
+MODIFY COLUMN warehouse_id CHAR(36) NULL;
---
+++ api/database/migrations/2025_04_08_071746_alter_servicelist_received_detail.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('servicelist_received_detail', function (Blueprint $table) {
+            $table->dropColumn('warehouse_id');
+            $table->char('warehouse_id', 36)->nullable()->after('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('servicelist_received_detail', function (Blueprint $table) {
+            $table->dropColumn('warehouse_id');
+            $table->char('warehouse_id', 36)->after('product_id');
+        });
+    }
+};
