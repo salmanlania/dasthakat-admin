756aa0871f24cc36d56ad1612a651e81f4b27ecf
noman830 <alirayyan.dev@gmail.com>
2025-04-20 22:34:59 +0500

    migration running issues

--- api/database/migrations/2025_03_10_103449_alter_certificate_id_in_certificate_detail_table.php
+++ api/database/migrations/2025_03_10_103449_alter_certificate_id_in_certificate_detail_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameCertifiateIdToCertificateId extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -19,4 +19,4 @@ class RenameCertifiateIdToCertificateId extends Migration
             $table->renameColumn('certificate_id', 'certifiate_id');
         });
     }
-}
+};
