8230edb4d19b71b03ac5e021f9e2c35aa1d428be
Rayyan Ali <alirayyan.dev@gmail.com>
2025-06-04 15:57:10 +0500

    block status default no in vessel and customer

--- api/database/alters/jun.sql
+++ api/database/alters/jun.sql
@@ -2,5 +2,5 @@ ALTER TABLE `sale_return` ADD COLUMN `ship_to` VARCHAR(255) AFTER `document_date
 ALTER TABLE `sale_return` ADD COLUMN `ship_via` VARCHAR(255) AFTER `ship_to`;
 ALTER TABLE `sale_return` ADD COLUMN `return_date` DATETIME AFTER `ship_via`;
 
-ALTER TABLE `vessel` ADD COLUMN `block_status` CHAR(36) AFTER `flag_id`;
-ALTER TABLE `customer` ADD COLUMN `block_status` CHAR(36) AFTER `phone_no`;
+ALTER TABLE `vessel` ADD COLUMN `block_status` CHAR(36) NOT NULL DEFAULT 'no' AFTER `flag_id`;
+ALTER TABLE `customer` ADD COLUMN `block_status` CHAR(36) NOT NULL DEFAULT 'no' AFTER `phone_no`;
