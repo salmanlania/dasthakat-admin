e45b3e83c9e1872830131fb172e01c73082e8ddb
DevRayyan <alirayyan.dev@gmail.com>
2025-06-11 14:57:20 +0500

    missing migrations created

---
+++ api/database/migrations/2025_06_09_160042_alter_purchase_invoice_detail_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('purhcase_invoice_detail', function (Blueprint $table) {
+            $table->char('purchase_order_detail_id', 36)->nullable()->after('purchase_invoice_detail_id');
+        });
+   
+
+    }
+
+    public function down(): void
+    {
+        
+        Schema::table('purhcase_invoice_detail', function (Blueprint $table) {
+            $table->dropColumn('purchase_order_detail_id');
+        });
+
+    }
+};
