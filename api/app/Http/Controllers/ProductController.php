94988056ccd5f508ae92d7f6ff2d7a4396960698
DevRayyan <alirayyan.dev@gmail.com>
2025-07-01 12:36:33 +0500

    product sale price send formated amount

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -77,6 +77,7 @@ class ProductController extends Controller
 
 		$query->select(
 			'product.*',
+    		DB::raw("TRIM(TRAILING '.' FROM TRIM(TRAILING '0' FROM FORMAT(product.sale_price, 2))) AS sale_price"),
 			"pt.name as product_type_name",
 			'c.name as category_name',
 			'sc.name as sub_category_name',
@@ -110,7 +111,9 @@ class ProductController extends Controller
 			->LeftJoin('brand as b', 'b.brand_id', '=', 'product.brand_id');
 
 		$product = $product->where('product.product_id', $id);
-		$product = $product->select('product.*', 'pt.product_type_id', 'pt.name as product_type_name', 'c.name as category_name', 'sc.name as sub_category_name', 'b.name as brand_name', 'u.name as unit_name')->first();
+		$product = $product->select('product.*',
+    		DB::raw("TRIM(TRAILING '.' FROM TRIM(TRAILING '0' FROM FORMAT(product.sale_price, 2))) AS sale_price"),
+		 'pt.product_type_id', 'pt.name as product_type_name', 'c.name as category_name', 'sc.name as sub_category_name', 'b.name as brand_name', 'u.name as unit_name')->first();
 		$product['image_url']  = !empty($product['image']) ?  url('public/uploads/' . $product['image']) : '';
 
 		$data = StockLedger::Check($product, $request->all());
@@ -126,7 +129,9 @@ class ProductController extends Controller
 			->LeftJoin('brand as b', 'b.brand_id', '=', 'product.brand_id');
 
 		$product = $product->where('product.product_code', $code);
-		$product = $product->select('product.*', 'c.name as category_name', 'sc.name as sub_category_name', 'b.name as brand_name', 'u.name as unit_name')->first();
+		$product = $product->select('product.*',
+    		DB::raw("TRIM(TRAILING '.' FROM TRIM(TRAILING '0' FROM FORMAT(product.sale_price, 2))) AS sale_price"),
+		 'c.name as category_name', 'sc.name as sub_category_name', 'b.name as brand_name', 'u.name as unit_name')->first();
 		$product['image_url']  = !empty($product['image']) ?  url('public/uploads/' . $product['image']) : '';
 		return $this->jsonResponse($product, 200, "Product Data");
 	}
