c623d7b62c68697a242bfe9196da0370c03f6090
DevRayyan <alirayyan.dev@gmail.com>
2025-09-17 15:44:19 +0500

    on create account hierarchy preview work

--- api/app/Http/Controllers/AccountsController.php
+++ api/app/Http/Controllers/AccountsController.php
@@ -96,34 +96,39 @@ class AccountsController extends Controller
     {
         $gl_type_id = $request->input('gl_type_id', '');
         $parent_account_id = $request->input('parent_account_id', '');
-        $data = Accounts::query();
 
-        if(!empty($gl_type_id)){
-            $data->where('gl_type_id',$gl_type_id);
+        $query = Accounts::query();
+
+        // If filtering by GL type
+        if (!empty($gl_type_id)) {
+            $query->where('gl_type_id', $gl_type_id);
         }
-        if(!empty($parent_account_id)){
-            $data->where('parent_account_id',$parent_account_id);
-        }
 
-        $data = $data->get();
+        // Always fetch ALL accounts matching GL type (so recursion works)
+        $accounts = $query->get()->toArray();
 
+        // Build map of accounts
         $map = [];
-        foreach ($data as $account) {
-            $accountArray = $account->toArray();
-            $accountArray['children'] = [];
-            $map[$accountArray['account_id']] = $accountArray;
+        foreach ($accounts as $acc) {
+            $acc['children'] = [];
+            $map[$acc['account_id']] = $acc;
         }
 
+        // Build hierarchy
         $tree = [];
         foreach ($map as $id => &$node) {
-            if ($node['parent_account_id'] == 0 || $node['parent_account_id'] == '' || $node['parent_account_id'] == null) {
-                $tree[] = &$node;
+            if (!empty($node['parent_account_id']) && isset($map[$node['parent_account_id']])) {
+                $map[$node['parent_account_id']]['children'][] = &$node;
             } else {
-                if (isset($map[$node['parent_account_id']])) {
-                    $map[$node['parent_account_id']]['children'][] = &$node;
-                }
+                $tree[] = &$node;
             }
         }
+
+        // If filtering by a parent account → extract its subtree
+        if (!empty($parent_account_id) && isset($map[$parent_account_id])) {
+            $tree = [$map[$parent_account_id]]; // return only this subtree
+        }
+
         return $this->jsonResponse($tree, 200, 'Accounts Data Tree');
     }
 
--- frontend/src/components/Form/CoaLevelForm.jsx
+++ frontend/src/components/Form/CoaLevelForm.jsx
@@ -4,14 +4,15 @@ import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getAccountsList, getAccountCode, getHeadAccountList } from '../../store/features/coaAccountsSlice';
+import { getAccountsList, getAccountCode, getHeadAccountList, getAccountsTree } from '../../store/features/coaAccountsSlice';
 import AsyncSelectLedger from '../AsyncSelectLedger';
 import DebounceInput from '../Input/DebounceInput';
+import AccountsTree from '../Tree/AccountsTree';
 
 const CoaLevelForm = ({ mode, onSubmit, onSave }) => {
   const [form] = Form.useForm();
   const dispatch = useDispatch();
-  const { isFormSubmitting, initialFormValues, initialFormCodeValues, isListLoading, headAccountList } = useSelector(
+  const { isFormSubmitting, initialFormValues, initialFormCodeValues, isListLoading, headAccountList, accountsTree, isTreeLoading } = useSelector(
     (state) => state.coaAccounts
   );
 
@@ -19,6 +20,8 @@ const CoaLevelForm = ({ mode, onSubmit, onSave }) => {
   const [data, setData] = useState([]);
   const [shouldSyncData, setShouldSyncData] = useState(true);
   const [accountType, setAccountType] = useState(null);
+  const [selectedGlTypeId, setSelectedGlTypeId] = useState(null);
+  const [selectedParentAccountId, setSelectedParentAccountId] = useState(null);
 
   const onFinish = (values) => {
     const data = {
@@ -60,6 +63,16 @@ const CoaLevelForm = ({ mode, onSubmit, onSave }) => {
       });
     }
   }, [initialFormValues, initialFormCodeValues, form, mode, headAccountList, shouldSyncData]);
+
+  // Fetch accounts tree when gl_type or parent_account changes
+  useEffect(() => {
+    if (selectedGlTypeId || selectedParentAccountId) {
+      dispatch(getAccountsTree({
+        gl_type_id: selectedGlTypeId,
+        parent_account_id: selectedParentAccountId
+      }));
+    }
+  }, [selectedGlTypeId, selectedParentAccountId, dispatch]);
 
   const columns = [
     {
@@ -148,121 +161,122 @@ const CoaLevelForm = ({ mode, onSubmit, onSave }) => {
             : { document_date: dayjs() }
         }
         scrollToFirstError>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+          <div className=''>
 
-        <Row gutter={12}>
-          <Col span={24} sm={12} md={6} lg={6}>
-            <Form.Item name="gl_types" label="Account Type">
-              <AsyncSelectLedger
-                endpoint="/lookups/gl-types"
-                valueKey="gl_type_id"
-                disabled={mode === 'edit' ? true : false}
-                labelKey="name"
-                labelInValue
-                className="w-full"
-                onChange={(selected) => {
-                  if (selected?.value) {
-                    dispatch(getAccountsList({ gl_type_id: selected.value }));
-                    setAccountType(selected);
-                    dispatch(getHeadAccountList({ gl_type_id: selected.value }));
-                  } else {
-                    setShouldSyncData(false);
-                    setData([])
-                  }
-                }}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={24} sm={12} md={8} lg={8}>
-            <Form.Item name="account_code" label="Account Number">
-              <Input
-                required
-                allowClear
-                disabled={mode === 'edit' ? true : false}
-                inputMode="numeric"
-                onKeyDown={(e) => {
-                  if (!/[0-9]/.test(e.key) && e.key !== "Backspace" && e.key !== "Delete" && e.key !== "ArrowLeft" && e.key !== "ArrowRight" && e.key !== "Tab") {
-                    e.preventDefault();
-                  }
-                }}
-                onPaste={(e) => {
-                  e.preventDefault();
-                  let pasteData = e.clipboardData.getData("text");
-                  pasteData = pasteData.slice(0, 3);
-                  const numbersOnly = pasteData.replace(/\D/g, "");
-                  if (numbersOnly) {
-                    const input = e.target;
-                    const start = input.selectionStart;
-                    const end = input.selectionEnd;
+            <Row gutter={12}>
+              <Col span={24} sm={12} md={8} lg={12}>
+                <Form.Item name="gl_types" label="Account Type">
+                  <AsyncSelectLedger
+                    endpoint="/lookups/gl-types"
+                    valueKey="gl_type_id"
+                    disabled={mode === 'edit' ? true : false}
+                    labelKey="name"
+                    labelInValue
+                    className="w-full"
+                    onChange={(selected) => {
+                      if (selected?.value) {
+                        dispatch(getAccountsList({ gl_type_id: selected.value }));
+                        setAccountType(selected);
+                        setSelectedGlTypeId(selected.value);
+                        dispatch(getHeadAccountList({ gl_type_id: selected.value }));
+                      } else {
+                        setShouldSyncData(false);
+                        setData([]);
+                        setSelectedGlTypeId(null);
+                      }
+                    }}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={24} sm={12} md={8} lg={12}>
+                <Form.Item name="account_code" label="Account Number">
+                  <Input
+                    required
+                    allowClear
+                    disabled={mode === 'edit' ? true : false}
+                    inputMode="numeric"
+                    onKeyDown={(e) => {
+                      if (!/[0-9]/.test(e.key) && e.key !== "Backspace" && e.key !== "Delete" && e.key !== "ArrowLeft" && e.key !== "ArrowRight" && e.key !== "Tab") {
+                        e.preventDefault();
+                      }
+                    }}
+                    onPaste={(e) => {
+                      e.preventDefault();
+                      let pasteData = e.clipboardData.getData("text");
+                      pasteData = pasteData.slice(0, 3);
+                      const numbersOnly = pasteData.replace(/\D/g, "");
+                      if (numbersOnly) {
+                        const input = e.target;
+                        const start = input.selectionStart;
+                        const end = input.selectionEnd;
 
-                    const newValue =
-                      input.value.substring(0, start) +
-                      numbersOnly +
-                      input.value.substring(end);
+                        const newValue =
+                          input.value.substring(0, start) +
+                          numbersOnly +
+                          input.value.substring(end);
 
-                    input.value = newValue.slice(0, 3);
+                        input.value = newValue.slice(0, 3);
 
-                    const event = new Event("input", { bubbles: true });
-                    input.dispatchEvent(event);
-                  }
-                }}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={24} sm={12} md={10} lg={10}>
-            <Form.Item name="coa_name" label="Account Name">
-              <Input required />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={12}>
-          <Col span={24} sm={12} md={6} lg={6}>
-            <Form.Item name="parent_account" label="Parent Account">
-              <AsyncSelectLedger
-                endpoint="/accounts"
-                valueKey="gl_type_id"
-                disabled={mode === 'edit' ? true : false}
-                labelKey="name"
-                labelInValue
-                className="w-full"
-                onChange={(selected) => {
-                  console.log(selected);
-                }}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={24} sm={12} md={6} lg={6}>
-            <Form.Item name="head_account" label="Head Account"> {/* pl statement */}
-              <AsyncSelectLedger
-                endpoint={`/accounts/account/heads?gl_type_id=${accountType?.value || ''}`}
-                valueKey="head_account_id"
-                disabled={mode === 'edit' ? true : false}
-                labelKey="head_account_name"
-                labelInValue
-                className="w-full"
-                onChange={(selected) => {
-                  form.setFieldsValue({ head_account: selected });
-                }}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        {/* {isListLoading ? (
-          <div className="flex min-h-32 items-center justify-center">
-            <Spin />
+                        const event = new Event("input", { bubbles: true });
+                        input.dispatchEvent(event);
+                      }
+                    }}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={24} sm={12} md={8} lg={12}>
+                <Form.Item name="coa_name" label="Account Name">
+                  <Input required />
+                </Form.Item>
+              </Col>
+           
+              <Col span={24} sm={12} md={8} lg={12}>
+                <Form.Item name="parent_account" label="Parent Account">
+                  <AsyncSelectLedger
+                    endpoint="/accounts"
+                    valueKey="account_id"
+                    disabled={mode === 'edit' ? true : false}
+                    labelKey="name"
+                    labelInValue
+                    className="w-full"
+                    onChange={(selected) => {
+                      setSelectedParentAccountId(selected?.value || null);
+                    }}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={24} sm={12} md={8} lg={12}>
+                <Form.Item name="head_account" label="Head Account"> {/* pl statement */}
+                  <AsyncSelectLedger
+                    endpoint={`/accounts/account/heads?gl_type_id=${accountType?.value || ''}`}
+                    valueKey="head_account_id"
+                    disabled={mode === 'edit' ? true : false}
+                    labelKey="head_account_name"
+                    labelInValue
+                    className="w-full"
+                    onChange={(selected) => {
+                      form.setFieldsValue({ head_account: selected });
+                    }}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
           </div>
-        ) : (
-          <Table
-            columns={columns}
-            dataSource={data}
-            rowKey={'head_account_id'}
-            size="small"
-            scroll={{ x: 'calc(100% - 200px)' }}
-            pagination={false}
-            sticky={{
-              offsetHeader: 56
-            }}
-          />
-        )} */}
+          <div>
+
+            {/* Accounts Tree View */}
+            {(selectedGlTypeId || selectedParentAccountId) && (
+                <AccountsTree
+                  treeData={accountsTree}
+                  loading={isTreeLoading}
+                  onSelect={(node) => {
+                  
+                  }}
+                />
+            )}
+          </div>
+        </div>
 
         <div className="mt-4 flex items-center justify-end gap-2">
           <Link to="/general-ledger/coa/level">
