8169e43bb0df83815a325db08c18a41179070cbd
azeemtariq0 <37283852+azeemtariq0@users.noreply.github.com>
2025-10-07 17:05:30 +0500

    doc no

--- api/resources/views/credit_note/header.blade.php
+++ api/resources/views/credit_note/header.blade.php
@@ -26,7 +26,7 @@
          <th> Date</th>
         <td class="text-left first">{{ \Carbon\Carbon::parse($document_date)->format('m-d-Y') }}</td>
       
-        <th>Doc no #</th>
+        <th>Doc #</th>
         <td class="text-left first">{{ $document_identity ?? "" }}</td>
       
          <th>Event No.</th>
