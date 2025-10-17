791bb21d9135db8b5ace00bde26ba7dd19003c46
Salman Lania <78688636+salmanlania@users.noreply.github.com>
2025-10-17 10:08:44 -0700

    revert

---
+++ frontend/.vite/deps/antd_es_layout_layout.js
@@ -0,0 +1,18 @@
+"use client";
+import {
+  Content,
+  Footer,
+  Header,
+  layout_default
+} from "./chunk-4EOG7WHK.js";
+import "./chunk-4PXUNI2B.js";
+import "./chunk-JMVEG3FK.js";
+import "./chunk-TWJRYSII.js";
+import "./chunk-DC5AMYBS.js";
+export {
+  Content,
+  Footer,
+  Header,
+  layout_default as default
+};
+//# sourceMappingURL=antd_es_layout_layout.js.map
